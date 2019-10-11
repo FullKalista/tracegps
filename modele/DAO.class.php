057d2cbd671aa2afef3e906b9873f0c959e5836a
delasalle-sio-couasnon-c <delasalle.sio.couasnon.c@gmail.com>
2019-10-11 11:19:54 +0200

    Méthode DAO.getUneTrace

--- modele/DAO.class.php
+++ modele/DAO.class.php
@@ -428,7 +428,42 @@ class DAO
     // Particularité : utiliser la méthode getLesPointsDeTrace($idTrace) pour obtenir les points de la trace 
     // et les ajouter à l'objet Trace qui sera retourné
     public function getUneTrace($idTrace) {
+        // préparation de la requête de recherche
+        $txt_req = "Select *";
+        $txt_req .= " from tracegps_traces";
+        $txt_req .= " where id = :idTrace";
+        $req = $this->cnx->prepare($txt_req);
+        // liaison de la requête et de ses paramètres
+        $req->bindValue("idTrace", utf8_decode($idTrace), PDO::PARAM_INT);
+        // extraction des données
+        $req->execute();
+        $uneLigne = $req->fetch(PDO::FETCH_OBJ);
+        // libère les ressources du jeu de données
+        $req->closeCursor();
         
+        // traitement de la réponse
+        if ( ! $uneLigne) {
+            return null;
+        }
+        else {
+            // création d'un objet Utilisateur
+            $unId = utf8_encode($uneLigne->id);
+            $uneDateHeureDebut = utf8_encode($uneLigne->dateDebut);
+            $uneDateHeureFin = utf8_encode($uneLigne->dateFin);
+            $terminee = utf8_encode($uneLigne->terminee);
+            $unIdUtilisateur = utf8_encode($uneLigne->idUtilisateur);
+            
+            $uneTrace = new Trace($unId, $uneDateHeureDebut, $uneDateHeureFin, $terminee, $unIdUtilisateur);
+            
+            $lesPDTs = DAO::getLesPointsDeTrace($idTrace);
+            
+            foreach ($lesPDTs as $unPointDeTrace)
+            {
+                $uneTrace->ajouterPoint($unPointDeTrace);
+            }
+            
+            return $uneTrace;
+        }
     }
     
     
